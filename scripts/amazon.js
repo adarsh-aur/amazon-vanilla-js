a2f07035286a261b9ed15be3fe9fcf50e036bd4b
Adarsh Ranjan <adarsh.aur@gmail.com>
2026-05-03 13:22:26 +0530

    Made add to cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,11 +46,37 @@ products.forEach((product) => {
         Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
-        Add to Cart
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+        Add to Cart 
         </button>
         </div>
     `;
     productsHTML += html;
-    document.querySelector(".js-product-grid").innerHTML = productsHTML;
+});
+
+document.querySelector(".js-product-grid").innerHTML = productsHTML;
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+    button.addEventListener("click", () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+
+        cart.forEach((item) => {
+            if(productId === item.productId{
+                matchingItem = item;
+            }
+        });
+
+        if(matchingItem) {
+            matchingItem.quantity++;
+        }
+        else {
+            cart.push({
+                productId: productId,
+                quantity: 1
+            })
+        }
+        console.log(cart)
+    });
 });
